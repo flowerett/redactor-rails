5e8d8423c5c05cdb030d6d3058fe3ba6bb0228c0
Nick Chernyshev <nick.chernyshev@gmail.com>
2014-01-27 16:42:11 +0600

    fix media resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RedactorRails::Engine.routes.draw do
   resources :pictures,  :only => [:index, :create]
   resources :documents, :only => [:index, :create]
-  resources :audio, :only => [:index, :create]
-  resources :video, :only => [:index, :create]
+  resources :audios, :only => [:index, :create]
+  resources :videos, :only => [:index, :create]
 end
